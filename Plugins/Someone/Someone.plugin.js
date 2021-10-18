a47e8dae8daeb2f4193c7ea24429c3c1a84190b5
HypedDomi <dominik.buettner1711@gmail.com>
2021-10-18 14:28:45 +0200

    Added settings

--- Plugins/Someone/Someone.plugin.js
+++ Plugins/Someone/Someone.plugin.js
@@ -2,7 +2,7 @@
  * @name Someone
  * @author HypedDomi#1711
  * @authorId 354191516979429376
- * @version 1.0
+ * @version 1.1
  * @description Brings back the @someone function
  * @source https://github.com/HypedDomi/BetterDiscordStuff/tree/main/Plugins/Someone
  * @updateUrl https://raw.githubusercontent.com/HypedDomi/BetterDiscordStuff/main/Plugins/Someone/Someone.plugin.js
@@ -22,16 +22,30 @@
                  discord_id: "354191516979429376",
              }
          ],
-         version: "1.0",
+         version: "1.1",
          description: "Brings back the @someone function",
          github: "https://github.com/HypedDomi/BetterDiscordStuff/tree/main/Plugins/Someone",
          github_raw: "https://raw.githubusercontent.com/HypedDomi/BetterDiscordStuff/main/Plugins/Someone/Someone.plugin.js"
      },
+     defaultConfig: [
+        {
+            type: "switch",
+            id: "ignoreSelf",
+            name: "Ignore yourself",
+            value: true
+        },
+        {
+            type: "switch",
+            id: "ignoreOffline",
+            name: "Ignore offline users",
+            value: true
+        }
+    ],
      changelog: [
         {
-            title: "1.0",
+            title: "NEW",
             type: "added",
-            items: ["The Plugin exist"]
+            items: ["Added settings"]
         }
     ]
  };
@@ -61,6 +75,12 @@
  } : (([Plugin, Library]) => {
     const {DiscordModules, Patcher} = Library;
     class Someone extends Plugin {
+        constructor() {
+            super();
+            this.getSettingsPanel = () => {
+               return this.buildSettingsPanel().getElement();
+           };
+        }
         onStart() {
             this.patchMessage();
         }
@@ -78,8 +98,15 @@
         getSomeone(msg) {
             let content = msg;
             let MemberStore = BdApi.findModuleByProps('getMembers', 'getMemberIds');
-            let Member = MemberStore.getMemberIds(BdApi.findModuleByProps('getLastSelectedGuildId').getLastSelectedGuildId())
-            Member = Member[Math.floor(Math.random() * Member.length)]
+            let Member = MemberStore.getMemberIds(BdApi.findModuleByProps('getLastSelectedGuildId').getLastSelectedGuildId());
+            if(this.settings.ignoreSelf) {
+                Member = Member.filter(e => e !== BdApi.findModuleByProps("getCurrentUser").getCurrentUser()["id"]);
+            }
+            Member = Member[Math.floor(Math.random() * Member.length)];
+            let status = BdApi.findModuleByProps("getStatus").getStatus(Member)
+            if(this.settings.ignoreOffline) {
+                if(status == "offline") return this.getSomeone(msg);
+            }            
             content = content.replace("@someone", `<@${Member}>`);
             return content;
         }
