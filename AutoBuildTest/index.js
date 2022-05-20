3dca2a83872b1afd0cb7a0e778aa67f663a5b5f1
HypedDomi <domi@bambus.me>
2022-05-20 10:21:27 +0200

    Added Template

--- AutoBuildTest/index.js
+++
@@ -1,7 +0,0 @@
-import BasePlugin from "@zlibrary/plugin";
-
-export default class AutoBuildTest extends BasePlugin {
-    onStart() {}
-
-    onStop() {}
-}
---
+++ Template/index.js
@@ -0,0 +1,7 @@
+import BasePlugin from "@zlibrary/plugin";
+
+export default class Template extends BasePlugin {
+    onStart() { }
+
+    onStop() { }
+}
