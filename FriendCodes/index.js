9a78739656f3d0f6db8c6f5b4ce2331407b4a9fa
HypedDomi <domi@bambus.me>
2022-05-24 11:56:10 +0200

    Update index.js

--- FriendCodes/index.js
+++ FriendCodes/index.js
@@ -1,5 +1,5 @@
 import BasePlugin from "@zlibrary/plugin";
-import { Patcher, ReactComponents } from "@zlibrary";
+import { Patcher, ReactComponents, WebpackModules } from "@zlibrary";
 import { openModal } from "@discord/modal";
 import React from "react";
 import styles from "styles";
@@ -9,18 +9,13 @@ import Modal from "./components/modal.jsx";
 export default class FriendCodes extends BasePlugin {
     async onStart() {
         styles.inject();
+        const TabBar = WebpackModules.getByProps("Item", "Header");
         const FriendsTabBar = await ReactComponents.getComponentByName("TabBar", ".tabBar-ra-EuL");
-        Patcher.after(FriendsTabBar.component.prototype, "render", (e, __, returnValue) => {
+        Patcher.after(FriendsTabBar.component.prototype, "render", (e, _, returnValue) => {
             // https://github.com/Strencher/BetterDiscordStuff/blob/development/RelationshipCounter/RelationshipCounter.plugin.js#L145
-            if(e.props.className && e.props.className.indexOf("tabBar-ra-EuL") !== -1) {
+            if (e.props.className && e.props.className.indexOf("tabBar-ra-EuL") !== -1) {
                 returnValue.props.children.push(
-                    React.createElement("div", {
-                        className: "item-3mHhwr item-3XjbnG themed-2-lozF",
-                        key: "friend-codes",
-                        onClick: () => {
-                            openModal(props => (<Modal {...props} />));
-                        }
-                    }, "Friend Codes")
+                    <TabBar.Item selectedItem={0} onClick={() => openModal(props => (<Modal {...props} />))}>Friend Codes</TabBar.Item>
                 );
             }
         });
