1743a4f3b29fc4c6f5463823bb6d2cf0343c8594
HypedDomi <domi@bambus.me>
2022-06-27 13:10:18 +0200

    Folder Support

--- AutoMarkAsRead/index.js
+++ AutoMarkAsRead/index.js
@@ -2,16 +2,39 @@ import BasePlugin from "@zlibrary/plugin";
 import { Patcher, ContextMenu, DiscordModules } from "@zlibrary";
 
 const AckUtils = BdApi.findModuleByProps("ack");
+const GuildFolders = BdApi.findModuleByProps("getGuildsTree").getCompatibleGuildFolders()
 export default class AutoMarkAsRead extends BasePlugin {
     onStart() {
+        this.patchFolderContextMenu();
         this.patchGuildContextMenu();
         this.patchChannelContextMenu();
         this.patchIncomingMessages();
     }
 
+    patchFolderContextMenu() {
+        ContextMenu.getDiscordMenu("GuildFolderContextMenu").then(cm => {
+            Patcher.after(cm, "default", (_, [{folderId}], ret) => {
+                const guilds = BdApi.getData("AutoMarkAsRead", "markAsReadGuilds") ?? [];
+                const guildsInFolder = GuildFolders.find(e => e.folderId == folderId).guildIds;
+                const autoMarkAsRead = guildsInFolder.some(id => guilds.includes(id));
+                const menu = ContextMenu.buildMenuChildren([{
+                    label: "AutoMarkAsRead",
+                    children: ContextMenu.buildMenuChildren([
+                        {
+                            label: autoMarkAsRead ? "Disable AutoMarkAsRead" : "Automaticly mark as read",
+                            danger: autoMarkAsRead,
+                            action: () => { BdApi.saveData("AutoMarkAsRead", "markAsReadGuilds", autoMarkAsRead ? guilds.filter(e => !guildsInFolder.includes(e)) : [...guilds, ...guildsInFolder]); }
+                        }
+                    ])
+                }]);
+                ret.props.children = [ret.props.children, menu];
+            });
+        })
+    }
+
     patchGuildContextMenu() {
-        ContextMenu.getDiscordMenu("GuildContextMenu").then(m => {
-            Patcher.after(m, "default", (_, [{ guild }], ret) => {
+        ContextMenu.getDiscordMenu("GuildContextMenu").then(cm => {
+            Patcher.after(cm, "default", (_, [{ guild }], ret) => {
                 const guilds = BdApi.getData("AutoMarkAsRead", "markAsReadGuilds") ?? [];
                 const autoMarkAsRead = guilds.includes(guild.id);
                 const menu = ContextMenu.buildMenuChildren([{
@@ -20,7 +43,7 @@ export default class AutoMarkAsRead extends BasePlugin {
                         {
                             label: autoMarkAsRead ? "Disable AutoMarkAsRead" : "Automaticly mark as read",
                             danger: autoMarkAsRead,
-                            action: () => { BdApi.saveData("AutoMarkAsRead", "markAsReadGuilds", autoMarkAsRead ? guilds.filter(c => c != guild.id) : [...guilds, guild.id]); }
+                            action: () => { BdApi.saveData("AutoMarkAsRead", "markAsReadGuilds", autoMarkAsRead ? guilds.filter(e => e != guild.id) : [...guilds, guild.id]); }
                         }
                     ])
                 }]);
@@ -30,8 +53,8 @@ export default class AutoMarkAsRead extends BasePlugin {
     }
 
     patchChannelContextMenu() {
-        ContextMenu.getDiscordMenu("useChannelDeleteItem").then(m => {
-            Patcher.after(m, "default", (_, [{ id }], ret) => {
+        ContextMenu.getDiscordMenu("useChannelDeleteItem").then(cm => {
+            Patcher.after(cm, "default", (_, [{ id }], ret) => {
                 const channels = BdApi.getData("AutoMarkAsRead", "markAsReadChannels") ?? [];
                 const autoMarkAsRead = channels.includes(id);
                 const menu = ContextMenu.buildMenuChildren([{
@@ -40,7 +63,7 @@ export default class AutoMarkAsRead extends BasePlugin {
                         {
                             label: autoMarkAsRead ? "Disable AutoMarkAsRead" : "Automaticly mark as read",
                             danger: autoMarkAsRead,
-                            action: () => { BdApi.saveData("AutoMarkAsRead", "markAsReadChannels", autoMarkAsRead ? channels.filter(c => c != id) : [...channels, id]); }
+                            action: () => { BdApi.saveData("AutoMarkAsRead", "markAsReadChannels", autoMarkAsRead ? channels.filter(e => e != id) : [...channels, id]); }
                         }
                     ])
                 }]);
