c587b5adea3297118c075c79d8de897e6d915eac
Dominik <me@domi-btnr.dev>
2023-07-12 18:08:24 +0200

    [ReplaceTimestamps] Switch to BdApi

--- Plugins/ReplaceTimestamps/README.md
+++ Plugins/ReplaceTimestamps/README.md
@@ -10,7 +10,7 @@
     <a href="https://discord.gg/gp2ExK5vc7">
         <img align="center" alt="Discord" src="https://img.shields.io/discord/838690163160514601?color=E54B4B&label=Support&logo=discord&logoColor=white&style=for-the-badge">
     </a>
-    <a href="https://paypal.me/dominik1711">
+    <a href="https://paypal.me/domibtnr">
         <img align="center" alt="PayPal" src="https://img.shields.io/badge/PayPal-Donate-ffffff?color=E54B4B&style=for-the-badge&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAgCAYAAAABtRhCAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAdhwAAHYcBj+XxZQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAIZSURBVEiJvZa7a1RREIe/CRsIqBBQJEEUO5+YRgSx0JTBwkKwsBMU/4HY2dmKFmm1tRBREEGFYGMhvgolPlBsRBuJj5iIiRvzWWxuOF7v3r2Ld/ODhXNmhvnOzHnshZzU82rTamqqn9XX6jV1XB3K52wrtaHOV4S104J6Ue2vAtz2n7BUt9S+PCNv2FG5HZ11GDi1mkCA052AO2sG7s63tdcV9gON1BDZQA1gFlhTI3AmIgZTQ1rhlpphAFN5Qwqse/8A7uYNaX/r3j+Am9lAHQbmegmcjIhnyfwosLFXwEXgbM52BBhemdl6hOvSeEpSR9QldTYzDNUE+q2eyZer3lj2rwBHa4A9UA8UwE4kMS+yPexm/xZpPRDTwBvgCXA9Ip4XwEaBicT0MHNMlKz8ijrQxYKynMfVn7lcxzLnZAnwYJegEfV2QZ6P6kDW0rJX5lUHwAZgD7CP1l3b2yb0XETMow6WVDedS35Bfbf8+2LrqFfRVVt/DqDuLwm8nwM+qghIdcnk+6aP8hO60s7lFW4va2+iReAOcCgiTkZEM3M0OgBfJuNNwLqCmHvAY+AbMAO8BZ5GxNeihA2qH5iiuO/AWET8Ksnxl/o6ANMKizox1Q0sA65t45sDPiTzov0rvTJFagC7gM0FvoWIMJkXVdg1sLLUTwVHfqxXsPVt7tjWbnP98+3fRkXt/AG8Xy3gEnA5Ipa6Bf4BbzmPtw84RMAAAAAASUVORK5CYII=">
     </a>
 </div>
--- Plugins/ReplaceTimestamps/ReplaceTimestamps.plugin.js
+++ Plugins/ReplaceTimestamps/ReplaceTimestamps.plugin.js
@@ -1,122 +1,51 @@
 /**
  * @name ReplaceTimestamps
- * @author HypedDomi#1711
+ * @author hypeddomi
  * @authorId 354191516979429376
- * @version 1.1.0
+ * @version 1.2.0
  * @description Replaces plaintext 24 hour timestamps into Discord's timestamps
  * @invite gp2ExK5vc7
  * @source https://github.com/HypedDomi/BetterDiscordStuff/tree/main/Plugins/ReplaceTimestamps
- * @updateUrl https://betterdiscord.app/gh-redirect/?id=
- * @donate https://paypal.me/dominik1711
- * @website https://bd.bambus.me/
+ * @updateUrl https://betterdiscord.app/gh-redirect/?id=699
+ * @donate https://paypal.me/domibtnr
  */
 
-const request = require("request");
-const fs = require("fs");
-const path = require("path");
+const BD = new BdApi("ReplaceTimestamps");
+const { Filters, getModule } = BD.Webpack;
 
-const config = {
-    info: {
-        name: "ReplaceTimestamps",
-        authors: [
-            {
-                name: "HypedDomi",
-                discord_id: "354191516979429376",
-            },
-        ],
-        version: "1.1.0",
-        description:
-            "Replaces plaintext 24 hour timestamps into Discord's timestamps",
-        github:
-            "https://github.com/HypedDomi/BetterDiscordStuff/tree/main/Plugins/ReplaceTimestamps",
-        github_raw:
-            "https://raw.githubusercontent.com/HypedDomi/BetterDiscordStuff/main/Plugins/ReplaceTimestamps/ReplaceTimestamps.plugin.js",
-    },
-    changelog: [
-        {
-            title: "AM/PM Support",
-            type: "added",
-            items: ["Added AM/PM support. To use, add `am` or `pm` to the end of the timestamp."],
-        }
-    ],
-};
+const MessageActionsFilter = Filters.byProps("jumpToMessage", "_sendMessage");
+const MessageActions = getModule(m => MessageActionsFilter(m));
 
-module.exports = !global.ZeresPluginLibrary
-    ? class {
-        constructor() {
-            this._config = config;
-        }
+const getUnixTimestamp = (time, format = "t") => {
+    const date = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "").replace(/\d?\d:\d\d/, time);
+    const then = Math.round((new Date(date)).getTime() / 1000);
+    if (isNaN(then)) return time;
+    return `<t:${then}:${format}>`;
+}
 
-        load() {
-            BdApi.showConfirmationModal(
-                "Library plugin is needed",
-                `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-                {
-                    confirmText: "Download",
-                    cancelText: "Cancel",
-                    onConfirm: () => {
-                        request.get(
-                            "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-                            (error, response, body) => {
-                                if (error)
-                                    return electron.shell.openExternal(
-                                        "https://betterdiscord.app/Download?id=9"
-                                    );
-
-                                fs.writeFileSync(
-                                    path.join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"),
-                                    body
-                                );
-                            }
-                        );
-                    },
+module.exports = meta => ({
+    start() {
+        BD.Patcher.before(MessageActions, "sendMessage", (_, [, msg]) => {
+            const regexAGlobal = /(?<!\d)\d{1,2}:\d{2}(?!\d)(am|pm)?(t|T|d|D|f|F|R)?/gi;
+            const regexA = /((?<!\d)\d{1,2}:\d{2}(?!\d))(am|pm)?(t|T|d|D|f|F|R)?/i;
+            if (msg.content.search(regexAGlobal) !== -1) msg.content = msg.content.replace(regexAGlobal, x => {
+                let [, time, mode, format] = x.match(regexA);
+                let [hours, minutes] = time.split(":").map(e => parseInt(e));
+                if (mode && mode.toLowerCase() === "pm" && hours < 12 && hours !== 0) {
+                    hours += 12;
+                    minutes = minutes.toString().padStart(2, "0");
+                    time = `${hours}:${minutes}`;
+                } else if ((mode && mode.toLowerCase() === "am" && hours === 12) || (hours === 24)) time = `00:${minutes}`;
+                else if (minutes >= 60) {
+                    hours += Math.floor(minutes / 60);
+                    minutes = (minutes % 60);
+                    time = `${hours}:${minutes}`;
                 }
-            );
-        }
-        start() {
-            this.load();
-        }
-        stop() {}
+                return getUnixTimestamp(time, format);
+            });
+        });
+    },
+    stop() {
+        BD.Patcher.unpatchAll();
     }
-    : (([Plugin, Library]) => {
-        const { Patcher, DiscordModules } = Library;
-        return class ReplaceTimestamps extends Plugin {
-            onStart() {
-                this.patchMessage();
-            }
-
-            patchMessage() {
-                Patcher.before(DiscordModules.MessageActions, "sendMessage", (_, [, msg]) => {
-                    const regexAGlobal = /(?<!\d)\d{1,2}:\d{2}(?!\d)(am|pm)?(t|T|d|D|f|F|R)?/gi;
-                    const regexA = /((?<!\d)\d{1,2}:\d{2}(?!\d))(am|pm)?(t|T|d|D|f|F|R)?/i;
-                    if (msg.content.search(regexAGlobal) !== -1) msg.content = msg.content.replace(regexAGlobal, x => {
-                        let [, time, mode, format] = x.match(regexA);
-                        format = format ?? "t";
-                        let [hours, minutes] = time.split(':').map(e => parseInt(e));
-                        if (mode && mode.toLowerCase() === 'pm' && hours < 12 && hours !== 0) {
-                            hours += 12;
-                            minutes = minutes.toString().padStart(2, '0');
-                            time = `${hours}:${minutes}`;
-                        } else if ((mode && mode.toLowerCase() === 'am' && hours === 12) || (hours === 24)) time = `00:${minutes}`;
-                        else if (minutes >= 60) {
-                            hours += Math.floor(minutes / 60);
-                            minutes = (minutes % 60);
-                            time = `${hours}:${minutes}`;
-                        }
-                        return this.getUnixTimestamp(time, format);
-                    });
-                });
-            }
-
-            getUnixTimestamp(time, format) {
-                const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/\d?\d:\d\d/, time);
-                const then = Math.round((new Date(date)).getTime() / 1000);
-                if (isNaN(then)) return time;
-                return `<t:${then}:${format}>`;
-            }
-
-            onStop() {
-                Patcher.unpatchAll();
-            }
-        }
-    })(global.ZeresPluginLibrary.buildPlugin(config));
+});
