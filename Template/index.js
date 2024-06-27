1d384c0914e45ed67f2971e4497b0b0e82942db8
Dominik <me@domi-btnr.dev>
2024-06-27 02:48:34 +0200

    Moving on

--- Template/index.js
+++
@@ -1,7 +0,0 @@
-import BasePlugin from "@zlibrary/plugin";
-
-export default class Template extends BasePlugin {
-    onStart() { }
-
-    onStop() { }
-}
--- types/common.d.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-
-declare type ValueOf<T> = T[keyof T];
-
-declare type UnknownFunction = (...args: any) => any;
