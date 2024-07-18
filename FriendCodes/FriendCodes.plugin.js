b7b7dec4253cdbed1df6e9c7c3e307c60f8ab542
domi-btnr <me@domi-btnr.dev>
2024-07-18 17:03:08 +0200

    Move Plugins to Root Folder

---
+++ FriendCodes/FriendCodes.plugin.js
@@ -0,0 +1,366 @@
+/**
+ * @name FriendCodes
+ * @version 1.0.2
+ * @description Generate FriendCodes to easily add friends
+ * @author domi.btnr
+ * @authorId 354191516979429376
+ * @invite gp2ExK5vc7
+ * @donate https://paypal.me/domibtnr
+ * @source https://github.com/domi-btnr/BetterDiscordStuff/tree/development/FriendCodes
+ * @changelogDate 2024-07-10
+ */
+
+'use strict';
+
+/* @module react */
+const React = BdApi.React;
+/*@end */
+
+/* @module @manifest */
+var manifest = {
+    "name": "FriendCodes",
+    "version": "1.0.2",
+    "description": "Generate FriendCodes to easily add friends",
+    "author": "domi.btnr",
+    "authorId": "354191516979429376",
+    "invite": "gp2ExK5vc7",
+    "donate": "https://paypal.me/domibtnr",
+    "source": "https://github.com/domi-btnr/BetterDiscordStuff/tree/development/FriendCodes",
+    "changelog": [{
+        "title": "Fixed",
+        "type": "fixed",
+        "items": ["Only show the Friend Codes Tab in the Friendlist"]
+    }],
+    "changelogDate": "2024-07-10"
+};
+/*@end */
+
+/* @module @api */
+const {
+    Components,
+    ContextMenu,
+    Data,
+    DOM,
+    Net,
+    Patcher,
+    Plugins,
+    ReactUtils,
+    Themes,
+    UI,
+    Utils,
+    Webpack
+} = new BdApi(manifest.name);
+/*@end */
+
+/* @module @styles */
+
+var Styles = {
+    sheets: [],
+    _element: null,
+    load() {
+        DOM.addStyle(this.sheets.join("\n"));
+    },
+    unload() {
+        DOM.removeStyle();
+    }
+};
+/*@end */
+
+/* @module style.scss */
+Styles.sheets.push("/* style.scss */", `.item {
+  color: #fff;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  border-radius: 8px;
+  padding: 10px;
+  margin-bottom: 10px;
+  background: var(--background-secondary);
+}
+.item code {
+  background: var(--background-tertiary);
+  padding: 2px;
+}
+.item div {
+  margin: 2px;
+}
+
+.buttonContainer {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.buttonContainer .buttonContainerInner {
+  display: block;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 10px;
+}
+.buttonContainer .buttonContainerInner button {
+  width: 80px;
+}
+
+.noInvites img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.noInvites h2 {
+  margin-top: 25px;
+  text-align: center;
+  font-weight: 500;
+  font-size: large;
+}`);
+var styles = {
+    "item": "item",
+    "buttonContainer": "buttonContainer",
+    "buttonContainerInner": "buttonContainerInner",
+    "noInvites": "noInvites"
+};
+/*@end */
+
+/* @module modal.jsx */
+const {
+    DiscordNative: {
+        clipboard: {
+            copy
+        }
+    }
+} = Webpack.getByKeys("DiscordNative");
+const {
+    Button,
+    Heading,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalRoot,
+    ModalSize
+} = Webpack.getByKeys("ModalContent");
+const {
+    createFriendInvite,
+    getAllFriendInvites,
+    revokeFriendInvites
+} = Webpack.getByKeys("createFriendInvite");
+const Flex = Webpack.getByStrings(".HORIZONTAL", ".START");
+const Markdown = Webpack.getByKeys("parseTopic");
+
+function CopyButton({
+    copyText,
+    copiedText,
+    onClick
+}) {
+    const [copied, setCopied] = React.useState(false);
+    const handleButtonClick = (e) => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1e3);
+        onClick(e);
+    };
+    return React.createElement(
+        Button, {
+            onClick: handleButtonClick,
+            color: copied ? Button.Colors.GREEN : Button.Colors.BRAND,
+            size: Button.Sizes.SMALL,
+            look: Button.Looks.FILLED
+        },
+        copied ? copiedText : copyText
+    );
+}
+
+function InviteCard(props) {
+    return React.createElement("div", {
+        className: styles.item
+    }, React.createElement("div", {
+        className: styles.code
+    }, React.createElement("span", null, React.createElement("b", null, "Code:"), " ", Markdown.parse(`\`${props.invite.code}\``))), React.createElement("div", {
+        className: styles.uses
+    }, React.createElement("span", null, React.createElement("b", null, "Uses:"), " ", props.invite.uses, "/", props.invite.max_uses)), React.createElement("div", {
+        className: styles.expiresAt
+    }, React.createElement("span", null, React.createElement("b", null, "Expires in:"), " ", Markdown.parse(`<t:${Math.floor(Date.parse(props.invite.expires_at).toString().slice(0, -3))}:R>`))), React.createElement("div", {
+        className: styles.buttonContainer
+    }, React.createElement("div", {
+        className: styles.buttonContainerInner
+    }, React.createElement(
+        CopyButton, {
+            copiedText: "Copied!",
+            copyText: "Copy",
+            onClick: () => copy(`https://discord.gg/${props.invite.code}`)
+        }
+    ))));
+}
+
+function Modal(props) {
+    const [invites, setInvites] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+    React.useEffect(() => {
+        setLoading(true);
+        getAllFriendInvites().then((invites2) => setInvites(invites2)).then(() => setLoading(false));
+    }, []);
+    return React.createElement(ModalRoot, {
+        ...props,
+        size: ModalSize.MEDIUM
+    }, React.createElement(ModalHeader, {
+        separator: false
+    }, React.createElement(Heading, {
+        level: "2",
+        variant: "heading-lg/medium"
+    }, "Friend Codes")), React.createElement(ModalContent, null, loading ? React.createElement("div", {
+        className: styles.loading
+    }) : invites.length > 0 ? invites.map((invite) => React.createElement(InviteCard, {
+        key: invite.code,
+        invite
+    })) : React.createElement("div", {
+        className: styles.noInvites
+    }, React.createElement("img", {
+        src: "https://discord.com/assets/b36c705f790dad253981f1893085015a.svg",
+        draggable: false
+    }), React.createElement(Heading, {
+        level: "3",
+        variant: "heading-lg/small"
+    }, "You don't have any friend codes yet"))), React.createElement(ModalFooter, null, React.createElement(Flex, {
+        justify: Flex.Justify.BETWEEN
+    }, React.createElement(Flex, {
+        justify: Flex.Justify.START
+    }, React.createElement(Button, {
+        color: Button.Colors.GREEN,
+        look: Button.Looks.OUTLINED,
+        onClick: () => createFriendInvite().then((invite) => setInvites([...invites, invite]))
+    }, "Create Friend Code"), React.createElement(Flex, {
+        justify: Flex.Justify.START
+    }, React.createElement(Button, {
+        color: Button.Colors.RED,
+        look: Button.Looks.LINK,
+        disabled: !invites.length,
+        onClick: () => revokeFriendInvites().then(setInvites([]))
+    }, "Revoke all Friend Codes"))), React.createElement(Button, {
+        onClick: props.onClose
+    }, "Okay"))));
+}
+
+/*@end */
+
+/* @module changelog.scss */
+Styles.sheets.push("/* changelog.scss */", `.Changelog-Title-Wrapper {
+  font-size: 20px;
+  font-weight: 600;
+  font-family: var(--font-display);
+  color: var(--header-primary);
+  line-height: 1.2;
+}
+.Changelog-Title-Wrapper div {
+  font-size: 12px;
+  font-weight: 400;
+  font-family: var(--font-primary);
+  color: var(--primary-300);
+  line-height: 1.3333333333;
+}
+
+.Changelog-Banner {
+  width: 405px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+
+.Changelog-Item {
+  color: #c4c9ce;
+}
+.Changelog-Item .Changelog-Header {
+  display: flex;
+  text-transform: uppercase;
+  font-weight: 700;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.Changelog-Item .Changelog-Header.added {
+  color: #45BA6A;
+}
+.Changelog-Item .Changelog-Header.fixed {
+  color: #EC4245;
+}
+.Changelog-Item .Changelog-Header.improved {
+  color: #5865F2;
+}
+.Changelog-Item .Changelog-Header::after {
+  content: "";
+  flex-grow: 1;
+  height: 1px;
+  margin-left: 7px;
+  background: currentColor;
+}
+.Changelog-Item span {
+  display: list-item;
+  list-style: inside;
+  margin-left: 5px;
+}
+.Changelog-Item span::marker {
+  color: var(--background-accent);
+}`); /*@end */
+
+/* @module index.jsx */
+const Settings = Data.load("SETTINGS") || {};
+class FriendCodes {
+    start() {
+        this.showChangelog();
+        this.patchFriendsTabBar();
+        Styles.load();
+    }
+    stop() {
+        Patcher.unpatchAll();
+        Styles.unload();
+    }
+    showChangelog() {
+        if (Settings.lastVersion === manifest.version) return;
+        const i18n = Webpack.getByKeys("getLocale");
+        const formatter = new Intl.DateTimeFormat(i18n.getLocale(), {
+            month: "long",
+            day: "numeric",
+            year: "numeric"
+        });
+        const title = React.createElement("div", {
+            className: "Changelog-Title-Wrapper"
+        }, React.createElement("h1", null, "What's New - ", manifest.name), React.createElement("div", null, formatter.format(new Date(manifest.changelogDate)), " - v", manifest.version));
+        const items = manifest.changelog.map((item) => React.createElement("div", {
+            className: "Changelog-Item"
+        }, React.createElement("h4", {
+            className: `Changelog-Header ${item.type}`
+        }, item.title), item.items.map((item2) => React.createElement("span", null, item2))));
+        "changelogImage" in manifest && items.unshift(
+            React.createElement("img", {
+                className: "Changelog-Banner",
+                src: manifest.changelogImage
+            })
+        );
+        Settings.lastVersion = manifest.version;
+        Data.save("SETTINGS", Settings);
+        UI.alert(title, items);
+    }
+    patchFriendsTabBar() {
+        const TabBar = Webpack.getModule((x) => x.Item && x.Header, {
+            searchExports: true
+        });
+        const availableTabs = Webpack.getByKeys("ALL", "BLOCKED", "ONLINE", {
+            searchExports: true
+        });
+        const {
+            openModal
+        } = Webpack.getModule((x) => x.openModal);
+        Patcher.after(TabBar.prototype, "render", (_, __, ret) => {
+            if (!(ret._owner.memoizedProps?.selectedItem in availableTabs)) return;
+            ret.props.children.push(
+                React.createElement(
+                    TabBar.Item, {
+                        selectedItem: 0,
+                        onClick: () => openModal((props) => React.createElement(Modal, {
+                            ...props
+                        }))
+                    },
+                    "Friend Codes"
+                )
+            );
+        });
+    }
+}
+
+/*@end */
+
+module.exports = FriendCodes;
---
+++ ReplaceTimestamps/ReplaceTimestamps.plugin.js
@@ -0,0 +1,430 @@
+/**
+ * @name ReplaceTimestamps
+ * @version 1.3.1
+ * @description Replaces plaintext times and dates into Discord's timestamps
+ * @author domi.btnr
+ * @authorId 354191516979429376
+ * @invite gp2ExK5vc7
+ * @donate https://paypal.me/domibtnr
+ * @source https://github.com/domi-btnr/BetterDiscordStuff/tree/development/ReplaceTimestamps
+ * @changelogDate 2024-07-13
+ */
+
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+/* @module react */
+const React = BdApi.React;
+/*@end */
+
+/* @module @manifest */
+var manifest = {
+    "name": "ReplaceTimestamps",
+    "version": "1.3.1",
+    "description": "Replaces plaintext times and dates into Discord's timestamps",
+    "author": "domi.btnr",
+    "authorId": "354191516979429376",
+    "invite": "gp2ExK5vc7",
+    "donate": "https://paypal.me/domibtnr",
+    "source": "https://github.com/domi-btnr/BetterDiscordStuff/tree/development/ReplaceTimestamps",
+    "changelog": [{
+            "title": "New Feature",
+            "type": "added",
+            "items": [
+                "Support for relative Times. Use \"in 5m\" or \"2h ago\""
+            ]
+        },
+        {
+            "title": "Valid Units",
+            "type": "improved",
+            "items": [
+                "\"s\" for seconds",
+                "\"m\" for minutes",
+                "\"h\" for hours",
+                "\"d\" for days",
+                "\"w\" for weeks",
+                "\"mo\" for months",
+                "\"y\" for years"
+            ]
+        }
+    ],
+    "changelogDate": "2024-07-13"
+};
+/*@end */
+
+/* @module @api */
+const {
+    Components,
+    ContextMenu,
+    Data,
+    DOM,
+    Net,
+    Patcher,
+    Plugins,
+    ReactUtils,
+    Themes,
+    UI,
+    Utils,
+    Webpack
+} = new BdApi(manifest.name);
+/*@end */
+
+/* @module @styles */
+
+var Styles = {
+    sheets: [],
+    _element: null,
+    load() {
+        DOM.addStyle(this.sheets.join("\n"));
+    },
+    unload() {
+        DOM.removeStyle();
+    }
+};
+/*@end */
+
+/* @module settings.js */
+const Dispatcher = Webpack.getByKeys("dispatch", "subscribe");
+const Flux = Webpack.getByKeys("Store");
+const Settings = new class Settings2 extends Flux.Store {
+    constructor() {
+        super(Dispatcher, {});
+    }
+    _settings = Data.load("SETTINGS") ?? {};
+    get(key, def) {
+        return this._settings[key] ?? def;
+    }
+    set(key, value) {
+        this._settings[key] = value;
+        Data.save("SETTINGS", this._settings);
+        this.emitChange();
+    }
+}();
+
+/*@end */
+
+/* @module settings.json */
+var SettingsItems = [{
+    type: "dropdown",
+    name: "Date Format",
+    note: "Select the date format for converting dates to timestamps",
+    id: "dateFormat",
+    options: [{
+            label: "dd.MM.yyyy",
+            value: "dd.MM.yyyy"
+        },
+        {
+            label: "dd/MM/yyyy",
+            value: "dd/MM/yyyy"
+        },
+        {
+            label: "MM.dd.yyyy",
+            value: "MM.dd.yyyy"
+        },
+        {
+            label: "MM/dd/yyyy",
+            value: "MM/dd/yyyy"
+        },
+        {
+            label: "yyyy.MM.dd",
+            value: "yyyy.MM.dd"
+        },
+        {
+            label: "yyyy/MM/dd",
+            value: "yyyy/MM/dd"
+        }
+    ],
+    value: "dd.MM.yyyy"
+}];
+/*@end */
+
+/* @module settings.jsx */
+const {
+    FormDivider,
+    FormText,
+    FormTitle,
+    Select
+} = Webpack.getByKeys("Select");
+
+function Dropdown(props) {
+    return React.createElement("div", {
+        style: {
+            marginBottom: "20px"
+        }
+    }, React.createElement(
+        FormTitle, {
+            tag: "h3",
+            style: {
+                margin: "0px",
+                color: "var(--header-primary)"
+            }
+        },
+        props.name
+    ), props.note && React.createElement(
+        FormText, {
+            type: FormText.Types.DESCRIPTION,
+            style: {
+                marginBottom: "5px"
+            }
+        },
+        props.note
+    ), React.createElement(
+        Select, {
+            closeOnSelect: true,
+            options: props.options,
+            serialize: (v) => String(v),
+            select: (v) => Settings.set(props.id, v),
+            isSelected: (v) => Settings.get(props.id, props.value) === v
+        }
+    ), React.createElement(FormDivider, {
+        style: {
+            marginTop: "20px"
+        }
+    }));
+}
+
+function renderSettings(items) {
+    return items.map((item) => {
+        switch (item.type) {
+            case "dropdown":
+                return React.createElement(Dropdown, {
+                    ...item
+                });
+            default:
+                return null;
+        }
+    });
+}
+
+function SettingsPanel() {
+    return React.createElement("div", {
+        className: "settings-panel"
+    }, renderSettings(SettingsItems));
+}
+
+/*@end */
+
+/* @module utils.js */
+const getUnixTimestamp = (str, format) => {
+    const timeMatch = str.match(exports.timeRegexMatch);
+    const dateMatch = str.match(exports.dateRegexMatch);
+    const formatParts = Settings.get("dateFormat", "dd.MM.yyyy").split(/[./]/);
+    let dayIndex, monthIndex, yearIndex;
+    formatParts.forEach((part, index) => {
+        if (part.includes("dd")) dayIndex = index;
+        if (part.includes("MM")) monthIndex = index;
+        if (part.includes("yyyy")) yearIndex = index;
+    });
+    let date = new Date();
+    if (dateMatch) {
+        let day = parseInt(dateMatch[dayIndex + 1]);
+        let month = parseInt(dateMatch[monthIndex + 1]);
+        let year = parseInt(dateMatch[yearIndex + 1]);
+        date = new Date(year, month - 1, day);
+    }
+    let time = date;
+    if (timeMatch) {
+        let [hours, minutes] = timeMatch[1].split(":").map((e) => parseInt(e));
+        if (timeMatch[2] && timeMatch[2].toLowerCase() === "pm" && hours < 12 && hours !== 0) {
+            hours += 12;
+            minutes = minutes.toString().padStart(2, "0");
+        } else if (timeMatch[2] && timeMatch[2].toLowerCase() === "am" && hours === 12 || hours === 24) {
+            hours = 0;
+        } else if (minutes >= 60) {
+            hours += Math.floor(minutes / 60);
+            minutes = (minutes % 60).toString().padStart(2, "0");
+        }
+        time = new Date(date);
+        time.setHours(hours);
+        time.setMinutes(minutes);
+    }
+    const then = Math.round(time.getTime() / 1e3);
+    if (isNaN(then)) return str;
+    return `<t:${then}${format ? `:${format}` : ""}>`;
+};
+const getRelativeTime = (str) => {
+    console.log(str);
+    const timeMatch = str.match(exports.relativeRegexMatch);
+    if (!timeMatch) return str;
+    const now = new Date();
+    let future = false;
+    let value, unit;
+    if (timeMatch[1] && timeMatch[2]) {
+        value = parseInt(timeMatch[1]);
+        unit = timeMatch[2];
+        future = true;
+    } else if (timeMatch[3] && timeMatch[4]) {
+        value = parseInt(timeMatch[3]);
+        unit = timeMatch[4];
+        future = false;
+    }
+    if (isNaN(value)) return str;
+    const adjustDate = (date, value2, unit2, future2) => {
+        switch (unit2.toLowerCase()) {
+            case "s":
+                date.setSeconds(date.getSeconds() + (future2 ? value2 : -value2));
+                break;
+            case "m":
+                date.setMinutes(date.getMinutes() + (future2 ? value2 : -value2));
+                break;
+            case "h":
+                date.setHours(date.getHours() + (future2 ? value2 : -value2));
+                break;
+            case "d":
+                date.setDate(date.getDate() + (future2 ? value2 : -value2));
+                break;
+            case "w":
+                date.setDate(date.getDate() + (future2 ? value2 * 7 : -value2 * 7));
+                break;
+            case "mo":
+                date.setMonth(date.getMonth() + (future2 ? value2 : -value2));
+                break;
+            case "y":
+                date.setFullYear(date.getFullYear() + (future2 ? value2 : -value2));
+                break;
+        }
+        return date;
+    };
+    const adjustedDate = adjustDate(now, value, unit, future);
+    const then = Math.round(adjustedDate.getTime() / 1e3);
+    return `<t:${then}:R>`;
+};
+
+/*@end */
+
+/* @module changelog.scss */
+Styles.sheets.push("/* changelog.scss */", `.Changelog-Title-Wrapper {
+  font-size: 20px;
+  font-weight: 600;
+  font-family: var(--font-display);
+  color: var(--header-primary);
+  line-height: 1.2;
+}
+.Changelog-Title-Wrapper div {
+  font-size: 12px;
+  font-weight: 400;
+  font-family: var(--font-primary);
+  color: var(--primary-300);
+  line-height: 1.3333333333;
+}
+
+.Changelog-Banner {
+  width: 405px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+
+.Changelog-Item {
+  color: #c4c9ce;
+}
+.Changelog-Item .Changelog-Header {
+  display: flex;
+  text-transform: uppercase;
+  font-weight: 700;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.Changelog-Item .Changelog-Header.added {
+  color: #45BA6A;
+}
+.Changelog-Item .Changelog-Header.fixed {
+  color: #EC4245;
+}
+.Changelog-Item .Changelog-Header.improved {
+  color: #5865F2;
+}
+.Changelog-Item .Changelog-Header::after {
+  content: "";
+  flex-grow: 1;
+  height: 1px;
+  margin-left: 7px;
+  background: currentColor;
+}
+.Changelog-Item span {
+  display: list-item;
+  list-style: inside;
+  margin-left: 5px;
+}
+.Changelog-Item span::marker {
+  color: var(--background-accent);
+}`); /*@end */
+
+/* @module index.jsx */
+exports.timeRegexMatch = void 0;
+exports.dateRegexMatch = void 0;
+exports.relativeRegexMatch = void 0;
+class ReplaceTimestamps {
+    start() {
+        this.showChangelog();
+        this.patchSendMessage();
+        Styles.load();
+    }
+    stop() {
+        Patcher.unpatchAll();
+        Styles.unload();
+    }
+    showChangelog() {
+        if (!manifest.changelog.length || Settings.get("lastVersion") === manifest.version) return;
+        const i18n = Webpack.getByKeys("getLocale");
+        const formatter = new Intl.DateTimeFormat(i18n.getLocale(), {
+            month: "long",
+            day: "numeric",
+            year: "numeric"
+        });
+        const title = React.createElement("div", {
+            className: "Changelog-Title-Wrapper"
+        }, React.createElement("h1", null, "What's New - ", manifest.name), React.createElement("div", null, formatter.format(new Date(manifest.changelogDate)), " - v", manifest.version));
+        const items = manifest.changelog.map((item) => React.createElement("div", {
+            className: "Changelog-Item"
+        }, React.createElement("h4", {
+            className: `Changelog-Header ${item.type}`
+        }, item.title), item.items.map((item2) => React.createElement("span", null, item2))));
+        "changelogImage" in manifest && items.unshift(
+            React.createElement("img", {
+                className: "Changelog-Banner",
+                src: manifest.changelogImage
+            })
+        );
+        Settings.set("lastVersion", manifest.version);
+        UI.alert(title, items);
+    }
+    patchSendMessage() {
+        const MessageActions = Webpack.getByKeys("sendMessage");
+        Patcher.before(MessageActions, "sendMessage", (_, [, msg]) => {
+            const timeRegex = /(?<!\d)\d{1,2}:\d{2}(?!\d)(am|pm)?/gi;
+            exports.timeRegexMatch = /((?<!\d)\d{1,2}:\d{2}(?!\d))(am|pm)?/i;
+            const dateFormat = Settings.get("dateFormat", "dd.MM.yyyy").replace(/[.\/]/g, "[./]").replace("dd", "(\\d{2})").replace("MM", "(\\d{2})").replace("yyyy", "(\\d{4})");
+            const dateRegex = new RegExp(`${dateFormat}`, "gi");
+            exports.dateRegexMatch = new RegExp(`${dateFormat}`, "i");
+            const TimeDateRegex = new RegExp(`(${timeRegex.source})\\s+${dateRegex.source}`, "gi");
+            const DateRegexTime = new RegExp(`${dateRegex.source}\\s+(${timeRegex.source})`, "gi");
+            const relativeRegex = /\b(?:in\s+(\d+)([smhdw]|mo|y)|(\d+)([smhdw]|mo|y)\s+ago)\b/gi;
+            exports.relativeRegexMatch = /\b(?:in\s+(\d+)([smhdw]|mo|y)|(\d+)([smhdw]|mo|y)\s+ago)\b/i;
+            if (msg.content.search(TimeDateRegex) !== -1) {
+                msg.content = msg.content.replace(TimeDateRegex, (x) => getUnixTimestamp(x));
+            }
+            if (msg.content.search(DateRegexTime) !== -1) {
+                msg.content = msg.content.replace(DateRegexTime, (x) => getUnixTimestamp(x));
+            }
+            if (msg.content.search(timeRegex) !== -1) {
+                msg.content = msg.content.replace(timeRegex, (x) => getUnixTimestamp(x, "t"));
+            }
+            if (msg.content.search(dateRegex) !== -1) {
+                msg.content = msg.content.replace(dateRegex, (x) => getUnixTimestamp(x, "d"));
+            }
+            if (msg.content.search(relativeRegex) !== -1) {
+                msg.content = msg.content.replace(relativeRegex, (x) => getRelativeTime(x));
+            }
+        });
+    }
+    getSettingsPanel() {
+        return React.createElement(SettingsPanel, null);
+    }
+}
+
+/*@end */
+
+exports.default = ReplaceTimestamps;
