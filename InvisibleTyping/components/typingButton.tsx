d040a63491f0a8bdb463caac8129b61e1af6e935
Dominik <me@domi-btnr.dev>
2024-08-29 02:17:49 +0200

    Test

---
+++ InvisibleTyping/components/typingButton.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Components, ContextMenu, UI } from "@api";
+import Keyboard from "./icons/keyboard";
+import styles from "./typingButton.scss";
+import Settings from "../modules/settings";
+import { buildClassName, TypingModule, useStateFromStores } from "../modules/shared";
+
+const removeItem = function (array: any[], item: any) {
+    while (array.includes(item)) {
+        array.splice(array.indexOf(item), 1);
+    }
+
+    return array;
+};
+
+function InvisibleTypingContextMenu() {
+    const enabled = useStateFromStores([Settings], () => Settings.get("autoEnable", true));
+
+    return (
+        <ContextMenu.Menu
+            navId="invisible-typing-context-menu"
+            onClose={ContextMenu.close}
+        >
+            <ContextMenu.Item
+                id="globally-disable-or-enable-typing"
+                label={enabled ? "Disable Globally" : "Enable Globally"}
+                action={() => {
+                    Settings.set("autoEnable", !enabled);
+                }}
+            />
+            <ContextMenu.Item
+                color="danger"
+                label="Reset Config"
+                disabled={!Settings.get("exclude", []).length}
+                id="reset-config"
+                action={() => {
+                    Settings.set("exclude", []);
+                    UI.showToast("Successfully reset config for all channels.", { type: "success" });
+                }}
+            />
+
+        </ContextMenu.Menu>
+    );
+}
+
+export default function InvisibleTypingButton({ channel, isEmpty }) {
+    const enabled = useStateFromStores([Settings], InvisibleTypingButton.getState.bind(this, channel.id));
+
+    const handleClick = React.useCallback(() => {
+        const excludeList = [...Settings.get<string[]>("exclude", [])];
+
+        if (excludeList.includes(channel.id)) {
+            removeItem(excludeList, channel.id);
+            TypingModule.stopTyping(channel.id);
+        } else {
+            excludeList.push(channel.id);
+            if (!isEmpty) TypingModule.startTyping(channel.id);
+        }
+
+        Settings.set("exclude", excludeList);
+    }, [enabled]);
+
+    const handleContextMenu = React.useCallback(event => {
+        ContextMenu.open(event, () => {
+            return <InvisibleTypingContextMenu />;
+        })
+    }, [enabled]);
+
+    return (
+        <Components.Tooltip text={enabled ? "Typing Enabled" : "Typing Disabled"}>
+            {props => (
+                <button
+                    {...props}
+                    className={
+                        buildClassName(styles.invisibleTypingButton, {
+                            enabled,
+                            disabled: !enabled
+                        })
+                    }
+                    onClick={handleClick}
+                    onContextMenu={handleContextMenu}
+                >
+                    <Keyboard disabled={!enabled} />
+                </button>
+            )}
+        </Components.Tooltip>
+    );
+}
+
+InvisibleTypingButton.getState = function (channelId: string) {
+    const isGlobal = Settings.get<boolean>("autoEnable", true);
+    const isExcluded = Settings.get("exclude", []).includes(channelId);
+
+    if (isGlobal && isExcluded) return false;
+    if (isExcluded && !isGlobal) return true;
+
+    return isGlobal;
+};
