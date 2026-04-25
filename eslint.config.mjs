89d379e8386605dcf85e81e1a619003a5be1b9b4
Dominik <me@domi-btnr.dev>
2026-04-25 23:12:10 +0200

    🚨 ✨  Add Prettier Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ const compat = new FlatCompat({
 export default [
     ...compat.extends("eslint:recommended"),
     ...compat.extends("plugin:react/recommended"),
-    { ignores: ["builds/**"] },
+    { ignores: ["builds/**", "archive/**"] },
     {
         settings: {
             react: {
@@ -44,7 +44,7 @@ export default [
                 ...globals.browser,
                 React: true,
                 JSX: true,
-                DiscordNative: true,
+                DiscordNative: true
             },
             parser: tsParser,
             parserOptions: {
@@ -62,51 +62,40 @@ export default [
         },
         rules: {
             "arrow-parens": ["error", "as-needed"],
+            "comma-dangle": ["error", "never"],
             "comma-spacing": "error",
             "comma-style": "error",
             "eol-last": ["error", "always"],
             "func-call-spacing": ["error", "never"],
-            "indent": ["error", 4, {
-                "SwitchCase": 1,
-            }],
+            indent: ["error", 4, { SwitchCase: 1 }],
             "keyword-spacing": "error",
-            "max-nested-callbacks": ["error", {
-                "max": 4,
-            }],
-            "max-statements-per-line": ["error", {
-                "max": 2,
-            }],
+            "max-len": ["error", { code: 120 }],
+            "max-nested-callbacks": ["error", { max: 4 }],
+            "max-statements-per-line": ["error", { max: 2 }],
             "no-console": "off",
             "no-empty-function": "error",
             "no-mixed-spaces-and-tabs": "error",
-            "no-multiple-empty-lines": ["error", {
-                "max": 2,
-                "maxEOF": 1,
-                "maxBOF": 0,
-            }],
+            "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 1, maxBOF: 0 }],
             "no-trailing-spaces": "error",
             "no-var": "error",
             "no-whitespace-before-property": "error",
+            "object-curly-spacing": ["error", "always"],
             "prefer-const": "error",
-            "quotes": ["error", "double"],
+            quotes: ["error", "double"],
             "react/jsx-key": "off",
             "react/no-unescaped-entities": "off",
             "react/prop-types": "off",
-            "semi": ["error", "always"],
+            semi: ["error", "always"],
             "space-before-blocks": "error",
-            "space-before-function-paren": ["error", {
-                "anonymous": "never",
-                "named": "never",
-                "asyncArrow": "always",
-            }],
+            "space-before-function-paren": ["error", { anonymous: "never", named: "never", asyncArrow: "always" }],
             "space-in-parens": "error",
             "space-infix-ops": "error",
             "space-unary-ops": "error",
             "spaced-comment": "error",
-            "yoda": "error",
+            yoda: "error",
             "simple-import-sort/imports": "error",
             "simple-import-sort/exports": "error",
-            "unused-imports/no-unused-imports": "error",
-        },
+            "unused-imports/no-unused-imports": "error"
+        }
     }
 ];
